06c9d74042cd058e07295e7acd87b417f54ea747
mariecf2 <mariecf2@illinois.edu>
2019-12-02 15:49:28 -0600

    heeyah

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button donezo = findViewById(R.id.donezoButton);
+        ImageButton redButton = findViewById(R.id.redButton);
+        ImageButton blueButton = findViewById(R.id.bluebutton);
+        ImageButton whiteButton = findViewById(R.id.whiteButton);
+        ImageButton hatButton = findViewById(R.id.hatButton);
+        ImageButton bootButton = findViewById(R.id.bootButton);
+        ImageButton micButton = findViewById(R.id.micButton);
+        ImageView dog = findViewById(R.id.dog);
+        ImageView redShirt = findViewById(R.id.redShirt);
+        ImageView blueShirt = findViewById(R.id.blueShirt);
+        ImageView whiteShirt = findViewById(R.id.whiteShirt);
+        ImageView hat = findViewById(R.id.hat);
+        ImageView boots = findViewById(R.id.boots);
+        ImageView mic = findViewById(R.id.mic);
     }
 }
