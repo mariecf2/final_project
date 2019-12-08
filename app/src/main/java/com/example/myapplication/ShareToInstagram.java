f852068028293704d2cb9c4d2fce8d748d36ced6
mariecf2 <mariecf2@illinois.edu>
2019-12-08 14:19:07 -0600

    ShareToInstagram.java

---
+++ app/src/main/java/com/example/myapplication/ShareToInstagram.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+
+import java.io.File;
+
+public class ShareToInstagram {
+    String type;
+    String filename;
+    String mediaPath;
+
+    ShareToInstagram(String setType, String setFilename, String setMediaPath) {
+        type = setType;
+        filename = setFilename;
+        mediaPath = setMediaPath;
+    }
+
+    public void onClickInsta() {
+        System.out.println("In OnClickInsta()");
+        createInstagramIntent(type, mediaPath);
+    }
+
+
+
+    private void createInstagramIntent(String type, String mediaPath){
+        System.out.println("In intent code bit");
+        // Create the new Intent using the 'Send' action.
+        Intent share = new Intent(Intent.ACTION_SEND);
+
+        // Set the MIME type
+        share.setType(type);
+
+        // Create the URI from the media
+        File media = new File(mediaPath);
+        Uri uri = Uri.fromFile(media);
+
+        // Add the URI to the Intent.
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+
+        // Broadcast the Intent.
+        //startActivity(Intent.createChooser(share, "Share to"));
+    }
+}
